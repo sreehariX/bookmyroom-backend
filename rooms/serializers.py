987905289ec7d5963be8238aec0cec4eca7ebd99
Arnav Prasad <arnavprasad@Arnavs-MacBook-Air-6.local>
2025-02-06 15:45:37 +0530

    Fixed Bug : Not able to send hostel name, you could only send the hostel id when creating a new room. Now you can send the hostel name.

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import serializers
 from .models import Room
+from hostels.models import Hostel
 
 class RoomSerializer(serializers.ModelSerializer):
     available_capacity_moodIndigo = serializers.SerializerMethodField()
     available_capacity_techFest = serializers.SerializerMethodField()
-    hostel_name = serializers.ReadOnlyField(source='hostel.name')
+    hostel_name = serializers.CharField(write_only=True)
+    hostel = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = ['id', 'room_number', 'capacity', 'people_booked_moodIndigo', 
+                 'people_booked_techFest', 'availability_status_moodIndigo',
+                'available_capacity_moodIndigo','availability_status_techFest',
+                 'available_capacity_techFest', 'hostel', 'hostel_name']
 
     def get_available_capacity_moodIndigo(self, obj):
         return obj.capacity - obj.people_booked_moodIndigo
 
     def get_available_capacity_techFest(self, obj):
         return obj.capacity - obj.people_booked_techFest
+
+    def create(self, validated_data):
+        hostel_name = validated_data.pop('hostel_name')
+        try:
+            hostel = Hostel.objects.get(name=hostel_name)
+        except Hostel.DoesNotExist:
+            raise serializers.ValidationError({"hostel_name": "Hostel not found"})
+        
+        validated_data['hostel'] = hostel
+        return super().create(validated_data)
